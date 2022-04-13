e42d593e80f4e89c403d9c4c7f7588816c00e7c8
Андрей Черников <ac@x-lab.com.ru>
2022-04-13 22:16:54 +0500

    Пример с кастомизацией инпута

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,39 +1,57 @@
-import React, { FunctionComponent } from 'react';
-import AccordionList from './AccordionList/AccordionList';
-import UserList from './firstHomework/UserList';
-import InputGeneric from './InputGeneric';
-import SecondTest from './secondHomework/SecondTest';
-import TabList from './Tabs/TabList';
+import React, { FunctionComponent } from 'react'
+import AccordionList from './AccordionList/AccordionList'
+import UserList from './firstHomework/UserList'
+import InputGeneric, {
+  InputGenericEditVersion,
+} from './InputGeneric'
+import SecondTest from './secondHomework/SecondTest'
+import TabList from './Tabs/TabList'
 
 interface Props {
-    isLoading: boolean
+  isLoading: boolean
 }
 
 const texts = {
-    default: "",
-    text1: 'text1',
-    text2: 'text2',
-    text3: 'text3',
-  }
-
-const Loader: FunctionComponent<Props> = ({ isLoading }) => {
-    let isVisible = (isLoading: boolean) => {
-        if (!isLoading) {
-            return <>
-            <UserList />
-            <SecondTest />
-            <AccordionList />
-            <TabList />
-            <InputGeneric texts={texts}/></>
-        } else {
-            return <p>Waiting...</p>
-        }
-    };
-    return (
-        <>
-            {isVisible(isLoading)}
-        </>
-    )
+  default: '',
+  text1: 'text1',
+  text2: 'text2',
+  text3: 'text3'
 }
 
-export default Loader;
+const Loader: FunctionComponent<Props> = ( { isLoading } ) => {
+  let isVisible = ( isLoading: boolean ) => {
+    if( !isLoading ) {
+      return <>
+        <UserList/>
+        <SecondTest/>
+        <AccordionList/>
+        <TabList/>
+        <InputGeneric
+          texts={texts}
+        />
+        <InputGenericEditVersion
+          selectData={[
+            { title: '123', id: '1', subTitle: '1', optionKey: 'Привет, Андрей', data: {value: 22, address: {street: 'Улица 1'}} },
+            { title: '123', id: '2', subTitle: '1', optionKey: 'Привет, Роман', data: {value: 22, address: {street: 'Улица 2'}} },
+            { title: '123', id: '3', subTitle: '1', optionKey: 'Привет, Евгений', data: {value: 22, address: {street: 'Улица 3'}} },
+            { title: '123', id: '4', subTitle: '1', optionKey: 'Привет, Дмитрий', data: {value: 22, address: {street: 'Улица 4'}} },
+          ]}
+          inSelectItemRenderKey={'optionKey'}
+          renderOption={(data) => {
+            return data.data.address.street
+          }}
+        />
+      </>
+
+    } else {
+      return <p>Waiting...</p>
+    }
+  }
+  return (
+    <>
+      {isVisible( isLoading )}
+    </>
+  )
+}
+
+export default Loader
