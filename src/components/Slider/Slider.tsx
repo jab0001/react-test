bc7ebae05c5f4889fa71a99e01a6756d767abc67
Андрей Черников <ac@x-lab.com.ru>
2022-04-16 14:32:59 +0500

    Правки по слайдерам

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,52 +1,62 @@
-import { FunctionComponent, useContext, useEffect, useState } from "react";
-import { ThemeContext } from "../Context/Context";
-import Slide from "./Slide";
-import SliderArrow from "./SliderArrow";
+import { FunctionComponent, useContext, useEffect, useState } from 'react'
+import { ThemeContext } from '../Context/Context'
+import Slide from './Slide'
+import SliderArrow from './SliderArrow'
 
 interface Props {
-    sliderData: Array<{
-        title: string
-    }>
+  sliderData: Array<{
+    title: string
+  }>,
+  initialCurrentSlide?: number
 }
 
+const Slider: FunctionComponent<Props> = ( { sliderData, initialCurrentSlide = 0 }: any ) => {
+  const [current, setCurrent] = useState<number>( initialCurrentSlide )
+  const [slide, setSlide] = useState( sliderData[ initialCurrentSlide ] )
+  // const [current, setCurrent] = useState<number>(0);
+  // const [slide, setSlide] = useState(sliderData[current])
+  const theme = useContext( ThemeContext )
 
-const Slider: FunctionComponent<Props> = ({ sliderData }: any) => {
-    const [current, setCurrent] = useState<number>(0);
-    const [slide, getSlide] = useState(sliderData[current])
-    const theme = useContext( ThemeContext )
-    
 
-    useEffect(
-        () => getSlide(sliderData[current]),
-        [current, slide]
-    )
+  useEffect(
+    () => {
+      setSlide( sliderData[ current ] )
+      console.log('вызов useEffect')
+    },
+    [current] //Удалить зависимость slide
+  )
 
-    const nextSlide = () => {
-        current === sliderData.length - 1 ?
-            setCurrent(0)
-            :
-            setCurrent(current + 1)
-    }
+  const nextSlide = () => {
+    current === sliderData.length - 1 ?
+      setCurrent( 0 )
+      :
+      setCurrent( current + 1 )
+    // setCurrent(current === (sliderData.length - 1) ? 0 : current + 1 )
 
-    const prevSlide = () => {
-        current === 0 ?
-            setCurrent(sliderData.length - 1)
-            :
-            setCurrent(current - 1)
-    }
+  }
 
-    return (
-        <div className="homework_wrapper">
-            <h2>Задание : Slider</h2>
-            <div className="slider" style={{ background: theme.background }}>
-                <Slide title={slide.title} />
-                <SliderArrow nextSlide={nextSlide}
-                    prevSlide={prevSlide} />
-            </div>
-        </div>
-    )
+  const prevSlide = () => {
+    // current === 0 ?
+    //   setCurrent( sliderData.length - 1 )
+    //   :
+    //   setCurrent( current - 1 )
+
+    setCurrent(!current ? sliderData.length - 1 : current - 1 )
+  }
+
+  return (
+    <div className="homework_wrapper">
+      <h2>Задание : Slider</h2>
+      <div className="slider" style={{ background: theme.background }}>
+        <Slide title={slide.title}/>
+        <SliderArrow
+          nextSlide={nextSlide}
+          prevSlide={prevSlide}
+        />
+      </div>
+    </div>
+  )
 }
-
 
 
 export default Slider
--- src/components/Slider/SliderRef.tsx
+++ src/components/Slider/SliderRef.tsx
@@ -1,46 +1,81 @@
-import { FunctionComponent, useRef, useState } from "react";
+import { FunctionComponent, useEffect, useRef, useState } from 'react'
 
 interface Props {
-    sliderData: Array<{
-        title: string
-    }>
+  sliderData: Array<{
+    title: string
+  }>
 }
 
 
-const SliderRef: FunctionComponent<Props> = ({ sliderData }) => {
-    const [currentSlide, setCurrent] = useState<number>(0);
-    const sliderPrev = useRef<HTMLButtonElement>(null)
-    const sliderNext = useRef<HTMLButtonElement>(null)
+const SliderRef: FunctionComponent<Props> = ( { sliderData } ) => {
+  const [currentSlide, setCurrent] = useState<number>( 0 )
+  const sliderPrev = useRef<HTMLButtonElement>( null )
+  const sliderNext = useRef<HTMLButtonElement>( null )
 
-    const Slide = (element:any) => {
-        if (element.current.className.includes("slider__arrow--next")) {
-            currentSlide === sliderData.length - 1 ?
-            setCurrent(0)
-            :
-            setCurrent(currentSlide + 1)
-        } else {
-            currentSlide === 0 ?
-            setCurrent(sliderData.length - 1)
-            :
-            setCurrent(currentSlide - 1)
-        }
+  useEffect( () => {
+    Slide( currentSlide )
+  }, [currentSlide] )
+
+  const ref = useRef<HTMLDivElement>( null )
+  const sliderContainerRef = useRef<HTMLDivElement>( null )
+  const Slide = ( currentSlide: number ) => {
+    // if( element.current.className.includes( 'slider__arrow--next' ) ) {
+    //   currentSlide === sliderData.length - 1 ?
+    //     setCurrent( 0 )
+    //     :
+    //     setCurrent( currentSlide + 1 )
+    // } else {
+    //   currentSlide === 0 ?
+    //     setCurrent( sliderData.length - 1 )
+    //     :
+    //     setCurrent( currentSlide - 1 )
+    // }
+
+    if(!sliderContainerRef.current){
+      return
     }
 
+    const width = sliderContainerRef.current.offsetWidth
+    const { current: currentElement } = ref
 
-    return (
-        <div className="homework_wrapper">
-            <h2>Задание : Slider</h2>
-            <div className="slider">
-                <div className="slider__item">
-                    <p className="slider__title">{sliderData[currentSlide].title}</p>
-                </div>
-                <button ref={sliderPrev} onClick={() =>Slide(sliderPrev)} className="slider__arrow slider__arrow--prev">&#10094;</button>
-                <button ref={sliderNext} onClick={() =>Slide(sliderNext)} className="slider__arrow slider__arrow--next">&#10095;</button>
+    const translateX = `${currentSlide * width}px`
+
+    if( currentElement ) {
+      currentElement.style.transform = `translateX(-${translateX})`
+    }
+
+  }
+
+
+  return (
+    <div className="homework_wrapper">
+      <h2>Задание : Slider</h2>
+      <div className="slider" ref={sliderContainerRef}>
+        <div className={'slider__body'} ref={ref}>
+          {sliderData.map( item => (
+            <div className="slider__item">
+              <p className="slider__title">
+                {item.title}
+              </p>
             </div>
+          ) )}
         </div>
-    )
+        <button
+          onClick={() => setCurrent( prev => prev - 1 < 0 ? 0 : prev - 1 )}
+          className="slider__arrow slider__arrow--prev"
+        >
+          &#10094;
+        </button>
+        <button
+          onClick={() => setCurrent( prev => prev + 1 > sliderData.length - 1 ? prev : prev + 1 )}
+          className="slider__arrow slider__arrow--next"
+        >
+          &#10095;
+        </button>
+      </div>
+    </div>
+  )
 }
-
 
 
 export default SliderRef
